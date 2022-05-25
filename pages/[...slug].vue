4669dbc5c2e229ac29502c6ad3ce7a1c912ab96f
Manuel Schröder <manuel.schroeder@storyblok.com>
2022-05-25 14:46:01 +0200

    update

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,8 +1,8 @@
 <script setup>
 const { slug } = useRoute().params
 
-// in your project, you will not need to add "part-2"
-const story = await useStoryblok(slug ? 'part-2/' + slug : 'part-2/home', {
+// in your project, you will not need to add "part-4"
+const story = await useStoryblok(slug ? 'part-4/' + slug : 'part-4/home', {
   version: 'draft',
 })
 </script>
