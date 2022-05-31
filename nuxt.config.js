7b87e02a739dd9a8226089c460738d6a9bb9e076
Manuel Schröder <manuel.schroeder@storyblok.com>
2022-05-31 13:59:48 +0200

    connect to default quickstart demo space

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
   modules: [
-    ['@storyblok/nuxt', { accessToken: 'NKTXbnJKkSyy1rYi68dXyQtt' }],
+    ['@storyblok/nuxt', { accessToken: 'OurklwV5XsDJTIE1NJaD2wtt' }],
     '@nuxtjs/tailwindcss',
   ],
 })
