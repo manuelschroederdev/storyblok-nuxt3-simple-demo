0268b2d9f17e451365d700c123dd88530f264bda
Manuel Schröder <manuel.schroeder@storyblok.com>
2022-06-03 10:56:07 +0200

    update

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,9 @@
   <header class="w-full h-24 bg-[#f7f6fd]">
     <div class="container h-full mx-auto flex items-center justify-between">
       <NuxtLink to="/">
-        <h1 class="text-[#50b0ae] text-3xl font-bold">Storyblok Nuxt</h1>
+        <h1 class="text-[#50b0ae] text-3xl font-bold">
+          Without StoryblokComponent
+        </h1>
       </NuxtLink>
       <nav>
         <ul class="flex space-x-8 text-lg font-bold">
